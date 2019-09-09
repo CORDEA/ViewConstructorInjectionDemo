e21c2f4b1b5491759e3f63c6a32ed824070953f8
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-09 19:17:25 +0900

    enable navigation

--- app/src/main/java/jp/cordea/viewconstructorinjectiondemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/viewconstructorinjectiondemo/MainActivity.kt
@@ -1,7 +1,7 @@
 package jp.cordea.viewconstructorinjectiondemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
